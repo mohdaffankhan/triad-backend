e75d90a000d78a505725e94f0b0869817b7e22df
rajul-triadacademy <rajulthakur34@gmail.com>
2026-02-03 23:44:44 +0530

    cors added for testing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 app.get('/', (req, res) => {
