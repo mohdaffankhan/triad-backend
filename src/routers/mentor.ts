44d5cf5cce0195f5e5e942570d7807b9c338f2d7
Affan Khan <mohdaffankhan@users.noreply.github.com>
2026-02-06 18:35:44 +0530

    fix typo

--- src/routers/mentor.ts
+++ src/routers/mentor.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { createMentor, deleteMentor, getAllMentor } from '../controllers/mentor.js';
+import { createMentor, deleteMentor, getAllMentors } from '../controllers/mentor.js';
 import { upload } from '../config/multer.js';
 
 const mentorRouter = express.Router();
 
 mentorRouter.route('/create').post(upload.single('image'), createMentor);
 mentorRouter.route('/delete/:mentorId').delete(deleteMentor);
-mentorRouter.route('/').get(getAllMentor);
+mentorRouter.route('/').get(getAllMentors);
 
 export default mentorRouter
